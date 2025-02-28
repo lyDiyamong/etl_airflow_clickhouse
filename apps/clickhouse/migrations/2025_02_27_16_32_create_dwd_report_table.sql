2e280994008e3eb72fa74faa33da795891cba625
lyDiyamong <diyamong26@gmail.com>
2025-02-28 09:49:52 +0700

    init : create dwd_report table'

--- apps/clickhouse/migrations/2025_02_27_16_32_create_dwd_report_table.sql
+++ apps/clickhouse/migrations/2025_02_27_16_32_create_dwd_report_table.sql
@@ -1,19 +1,20 @@
 CREATE Table IF NOT EXISTS clickhouse.dwd_report {
     eventName String,
-    studentDetails Array(
+    studentDetails 
         Tuple(
             male UInt64
             female UInt64
             other UInt64
             total UInt64
-        )
-    ),
-    teacherDetail Array(
+        ),
+    
+    teacherDetail 
         Tuple(
             male UInt64
             female UInt64
             other UInt64
             total UInt64
-        )
-    )
+        ),
+    eventCount UInt64
+    
 }
